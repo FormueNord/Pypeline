01a85c68c0327752af7604f8756ddcda5d9fe45e
ChrisAlbertsen <chrisdalbertsen@gmail.com>
2022-05-11 14:52:00 +0200

    Removed time from stamps so now only datestamp

--- transformer_functions.py
+++ transformer_functions.py
@@ -9,7 +9,7 @@ def function_constructor(func):
         return lambda file_paths: func(file_paths, **kwargs)
     return wrapper
 
-def strings_to_dates(df,datetime_transformer = lambda x: datetime.datetime.strptime(x,"%d/%m/%Y")):
+def strings_to_dates(df,datetime_transformer = lambda x: datetime.datetime.strptime(x,"%d/%m/%Y").date()):
     for column in df.columns:
         if "date" in column.lower():
             df[column] = df[column].apply(datetime_transformer)
