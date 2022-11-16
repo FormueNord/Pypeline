440e0271e7841d4b95d57fc720c3b39da8ad58bc
ChrisAlbertsen <chrisdalbertsen@gmail.com>
2022-11-16 09:52:16 +0100

    Add: comments regarding failed test

--- tests/test_ErrorAlerter.py
+++ tests/test_ErrorAlerter.py
@@ -2,12 +2,16 @@ from Pypeline.ErrorAlerter import ErrorAlerter
 import pytest
 
 
+"""
+If these tests fail, and its due to no credentials are being stored simply run the script and add some new credentials.
+"""
 def test_authentication():
     try:
         # email is old throwaway email. Spam it all you want
         alerter = ErrorAlerter("chris.albertsen90@gmail.com", "TEST ERROR ALERT FROM PYPELINE", "ERROR ALERT EXAMPLE TEST")
-    except Exception as e:  
-        assert False, f"Failed to initialize an ErrorAlerter obj with error message {e}"
+    except Exception as e:
+        assert False, f"""Failed to initialize an ErrorAlerter obj with error message {e} .
+                        The issue might be that no credentials are stored in the test folder for the ErrorAlerter"""
     return alerter
 
 alerter = test_authentication()
@@ -15,3 +19,5 @@ alerter = test_authentication()
 def test_error_alert():
     result = alerter.error_alert()
     assert result == None, f"failed to send error alert using SMTP with error code(s){result}"
+
+
