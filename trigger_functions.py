aefc7f90ded654c3a27ca14bf81fa47a298cabd3
ChrisAlbertsen <chrisdalbertsen@gmail.com>
2023-03-14 13:48:33 +0100

    add: replace with regex string search

--- trigger_functions.py
+++ trigger_functions.py
@@ -4,6 +4,7 @@ import os.path
 import email
 from datetime import timedelta, datetime
 import Othenticator
+import re
 
 def function_constructor(func):
     """
@@ -20,14 +21,16 @@ def folder_monitor_constructor(folder_path, in_name_criteria = None):
 
     Args:
         folder:  specify path to monitored folder
-        in_name_criteria:  filter out any files in folder not containing the criteria in name
+        in_name_criteria:  filter out any files in folder not containing the Regex criteria in name
 
     Returns:
         List of file names in folder containing criteria (all files if no criteria is set) 
     """
+
+
     files_in_folder = os.listdir(folder_path)
     if in_name_criteria:
-        trigger_files = [file for file in files_in_folder if in_name_criteria in file]
+        trigger_files = [file for file in files_in_folder if re.match(in_name_criteria,file)]
         trigger_files = [os.path.join(folder_path,file) for file in trigger_files]
         return trigger_files
     return [os.path.join(folder_path,file) for file in files_in_folder]
